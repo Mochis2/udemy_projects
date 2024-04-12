670e9a4e8341c5f44efc824a29644a25076a61cf
Maurice <110825969+Mochis2@users.noreply.github.com>
2024-04-12 21:30:30 +0200

    Update main.py

--- day_33_api/main.py
+++ day_33_api/main.py
@@ -1,16 +1,6 @@
 import requests
 from datetime import datetime
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-
-# data = response.json()
-
-
-# longitude = data['iss_position']['longitude']
-# latitude = data['iss_position']['latitude']
-
-# iss_position = (longitude, latitude)
 
 MY_LAT = 47.499882
 MY_LONG = 8.726160
